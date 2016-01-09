5b46dfcf8412382bb1056e39680d5b9c9b463d28
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-01-09 07:34:01 +0100

    Template finished

--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
 (function(w,d) {
+    var menu_state = 0;
 
     function init() {
-        var responsive_menu_button = document.getElementById("tn-nav-mobile-btn"),
-            menu_container = document.getElementById("tn-nav-opt-container");
 
-        responsive_menu_button.addEventListener('click', function(){
-            menu_container.classList.add('open');
-            menu_container.classList.remove('closed');
+        var responsive_menu_button = document.getElementById("tn-nav-mobile-btn"),
+        col_container = document.getElementsByClassName("col-container");
+
+        for (var i= 0,z = col_container.length; i < z; i++) {
+
+            col_container[i].addEventListener('click', function(){
+                 closeMenu();
+            });
+
+        }
+
+
+
+
+        responsive_menu_button.addEventListener('click', function() {
+
+            if (menu_state === 0) {
+                openMenu();
+            } else {
+                closeMenu();
+            }
+
         });
 
+    };
+
+    function openMenu() {
+        var menu_container = document.getElementById("tn-nav-opt-container");
+        menu_container.classList.add('open');
+        menu_container.classList.remove('closed');
+
+        menu_state = 1;
+    };
+
+    function closeMenu() {
+        var menu_container = document.getElementById("tn-nav-opt-container");
+        menu_container.classList.add('closed');
+        menu_container.classList.remove('open');
+
+        menu_state = 0;
     };
 
     d.addEventListener('DOMContentLoaded', init);
